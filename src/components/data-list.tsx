7de8b0fc705c3214c5937885e9e0eae07013da60
donatedsalt <mmohsinkhan3685@gmail.com>
2025-07-29 17:50:10 +0500

    add data-list loading skeleton

--- src/components/data-list.tsx
+++ src/components/data-list.tsx
@@ -10,13 +10,17 @@ import { ITransaction } from "@/types/transaction";
 
 import { cn } from "@/lib/utils";
 
-import { Avatar } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
+import { Avatar } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export function DataList({ data }: { data: ITransaction[] }) {
   return (
-    <Tabs defaultValue="all" className="flex-col justify-start w-full gap-4">
+    <Tabs
+      defaultValue="all"
+      className="flex-col justify-start w-full gap-4 md:gap-6"
+    >
       <div className="px-4 lg:px-6">
         <TabsList>
           <TabsTrigger value="all">All</TabsTrigger>
@@ -25,7 +29,7 @@ export function DataList({ data }: { data: ITransaction[] }) {
         </TabsList>
       </div>
       <TabsContent value="all" className="flex flex-col px-4 lg:px-6">
-        <ul>
+        <ul className="grid gap-4">
           {data.map((item, index) => (
             <ListItem key={index} item={item} />
           ))}
@@ -43,7 +47,7 @@ export function DataList({ data }: { data: ITransaction[] }) {
 
 export function ListItem({ item }: { item: ITransaction }) {
   return (
-    <li className="flex items-center justify-between gap-2 py-4">
+    <li className="flex items-center justify-between gap-2">
       <div className="flex items-center gap-2">
         <Avatar className="items-center justify-center bg-neutral-100 text-foreground size-12">
           <ListItemIcon item={item.type} />
@@ -79,4 +83,36 @@ function ListItemIcon({ item }: { item: string }) {
     default:
       return null;
   }
+}
+
+export function DataListSkeleton() {
+  return (
+    <div className="flex flex-col justify-start w-full gap-4 md:gap-6">
+      <div className="px-4 lg:px-6">
+        <Skeleton className="h-6 w-42" />
+      </div>
+      <div className="flex flex-col px-4 lg:px-6">
+        <ul className="grid gap-4">
+          {[...Array(12)].map((_, i) => (
+            <ListItemSkeleton key={i} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export function ListItemSkeleton() {
+  return (
+    <li className="flex items-center justify-between gap-2">
+      <div className="flex items-center gap-2">
+        <Skeleton className="rounded-full size-12" />
+        <div className="grid gap-2">
+          <Skeleton className="w-24 h-3" />
+          <Skeleton className="w-12 h-2" />
+        </div>
+      </div>
+      <Skeleton className="w-16 h-4" />
+    </li>
+  );
 }
